8b9ea4be502e974cfc040e0d5aba8adf8cf7e857
Zsolt Makrai <mecrai@gmail.com>
2017-04-10 09:30:31 +0200

    feat(seconds): retunr every second element in the list)

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,14 @@
-def seconds(change_this):
-    pass
+
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
+# print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
 
-print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+def seconds(numbers):
+    seconds = []
+    for number in numbers[1::2]:
+        seconds.append(number)
+    return(seconds)
+
+
+print(seconds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
