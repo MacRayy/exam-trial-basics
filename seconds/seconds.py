61a15a620e34dc67aa7d9e7db1ac74a107185361
Zsolt Makrai <mecrai@gmail.com>
2017-04-10 10:29:47 +0200

    (seconds): tested with different list

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -11,4 +11,4 @@ def seconds(numbers):
     return(seconds)
 
 
-print(seconds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
+print(seconds(["apple", "orange", "lemon", "banana", "pear", "strawberry"]))
